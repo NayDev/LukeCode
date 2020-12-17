acfeb6edf7388e226b7b83e54861157fd84ac9c9
Nayara <nayara.lopes2007@gmail.com>
2020-12-17 18:07:52 -0300

    Fazendo segunda e terceira section.

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './styles.css';
 
 import Animation from '../../components/Animation';
+import CardCourse from '../../components/CardCourse';
+
 import yoda from '../../assets/images/characters/yoda.png';
 import separator from '../../assets/images/separator.svg';
+
+import courseReactRedux from '../../assets/images/courses/react-redux.jpg';
+import coursePhp from '../../assets/images/courses/php.jpg';
+import courseFlutter from '../../assets/images/courses/flutter.jpg';
 
 
 const Home = () => {
     return(
         <>
-        <Animation />
+        {/* <Animation /> */}
             <div className="section-1">
                 <div className="container">
                     <header className="header">
@@ -32,8 +38,47 @@ const Home = () => {
                     </main>
                 </div>
             </div>
+            {/* Segunda seção */}
+            <div className="section-2">
+                <div className="container">
+                    <h2>+3 Cursos completos</h2>
+                    <p>
+                        Na Luke code, você tem acesso a 3 cursos gratuitos, basta se cadastrar na plataforma.
+                    </p>
+                    <section className="card-container">
+                    <CardCourse image={courseReactRedux} description={"Desenvolvimento front-end React e Redux"}>
+                        Desenvolvimento front-end <br /> React e Redux
+                    </CardCourse>
+                    <CardCourse image={coursePhp}>
+                        PHP 7 - Trabalhando com PSRs
+                    </CardCourse>
+                    <CardCourse image={courseFlutter}>
+                       Desenvolvimento IOS e Android <br /> com Flutter                      
+                    </CardCourse>
+                    </section>
+                    <button>INSCREVA-SE</button>
+                </div>
+            </div>
+            <div className="section-3">
+                <div className="container">
+                    <h2>O que oferecemos para você</h2>
+                    <section className="categories-container">
+
+                        <div className="category-card">
+                            <div className="category-card-image">
+                                {/* <img src="" alt=""/> */}
+                            </div>
+                            <div className="category-card-content">
+                                <h4>BACK-END</h4>
+                                <p>22 Cursos</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </>
     )
 }
 
 export default Home;
+
